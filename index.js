a68cb4d8bd576e78ae5cb302c5017717166dd085
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-12-27 17:26:48 +0100

    Some additional error handling.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const Note = require("./models/note");
+console.log(Note)
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
+
+
 app.use(express.static("build"));
 
 app.get("/", (request, response) => {
@@ -32,8 +35,17 @@ app.get("/api/notes", (request, response) => {
 
 app.get("/api/notes/:id", (request, response) => {
   Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+    if(note){
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
+
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).send({error: 'malformatted id'})
+  })
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -76,6 +88,19 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
