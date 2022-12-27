2239f0732deb6649ed003f83e218ab7120a7863b
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-12-27 11:04:56 +0100

    Adapted the db name and included the functionality to add new notes.

--- mongo.js
+++ mongo.js
@@ -4,12 +4,17 @@ if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
+if (process.argv.length < 4) {
+  console.log('Please provide the text for the new note to be included')
+  process.exit(1)
+}
 
 const password = process.argv[2]
+const newNote = process.argv[3]
 
 
 // important: the name of the database has to be included BEFORE the question mark ?retryWrites...
-const url = `mongodb+srv://fullstack:${password}@cluster0.pf1gfrd.mongodb.net/test?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.pf1gfrd.mongodb.net/noteBook?retryWrites=true&w=majority`
 
 
 
@@ -27,7 +32,7 @@ mongoose
     console.log('connected')
 
     const note = new Note({
-      content: 'HTML is Easy',
+      content: newNote,
       date: new Date(),
       important: true,
     })
@@ -35,7 +40,7 @@ mongoose
     return note.save()
   })
   .then(() => {
-    console.log('note saved!')
+    console.log(newNote,'note saved!')
     return mongoose.connection.close()
   })
   .catch((err) => console.log(err))
